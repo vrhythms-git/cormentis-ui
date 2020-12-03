94584eb024c462bc8006762693fca3d65ea92095
vrhythms-git <65596801+vrhythms-git@users.noreply.github.com>
2020-12-03 12:40:50 +0530

    updated port number

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '//dist//cormentis'));
+let port = 8081;
+app.use(express.static(__dirname + '/dist/cormentis'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+"//dist//cormentis//index.html"));
+res.sendFile(path.join(__dirname+"/dist//cormentis/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+
+app.listen(process.env.PORT || port,
+    ()=>{
+        console.log(`express server started at port : ${port}`)
+});
