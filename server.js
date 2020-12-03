ca5e6ab38862349c307ba2f792c8cd8179694e12
vrhythms-git <65596801+vrhythms-git@users.noreply.github.com>
2020-12-03 13:43:50 +0530

    adding express to package.json

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-let port = 8080;
+let port = 8081;
 app.use(express.static(__dirname + '/dist/cormentis'));
 
 app.get('/*', function(req,res) {
